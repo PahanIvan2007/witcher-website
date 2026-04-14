dd46efbc0761a457a87980c43109d5998c05f886
IVAN <126900030+PahanIvan2007@users.noreply.github.com>
2026-04-14 22:13:24 +0300

    Update character images

--- src/app/pages/characters.component.ts
+++ src/app/pages/characters.component.ts
@@ -68,11 +68,11 @@ export class CharactersComponent implements OnInit {
 
   characters = [
     { id: 1, name: 'Геральт из Ривии', aka: 'БЕЛОВОЛОСЫЙ', description: 'Беловолосый ведьмак из Школы Волка. Наёмный убийца чудовищ.', weapon: 'Стальной и Серебряный мечи', origin: 'Каэр Морхен', image: 'assets/images/a5fb7a948cb613a6db107e4e998684cc.jpg' },
-    { id: 2, name: 'Цири', aka: 'ЛАСТОЧКА', description: 'Принцесса Цинтры, носительница Elder Blood. Дочь Паветты.', weapon: 'Меч из Ласточки', origin: 'Цинтра', image: 'assets/images/h280_39780019.jpg' },
-    { id: 3, name: 'Йеннифер', aka: 'ЛЬВИЦА', description: 'Чёрная чародейка. Одна из самых могущественных магов Севера.', weapon: 'Магия хаоса', origin: 'Аретуза', image: 'assets/images/a652dcfff029f1f9f5e5e9bf06ee1622.jpg' },
-    { id: 4, name: 'Лютик', aka: 'БАРД', description: 'Поэт и друг Геральта. Лучший бард Севера.', weapon: 'Лютня', origin: 'Новиград', image: 'assets/images/i (8).webp' },
+    { id: 2, name: 'Цири', aka: 'ЛАСТОЧКА', description: 'Принцесса Цинтры, носительница Elder Blood. Дочь Паветты.', weapon: 'Меч из Ласточки', origin: 'Цинтра', image: 'assets/images/i (10).webp' },
+    { id: 3, name: 'Йеннифер', aka: 'ЛЬВИЦА', description: 'Чёрная чародейка. Одна из самых могущественных магов Севера.', weapon: 'Магия хаоса', origin: 'Аретуза', image: 'assets/images/i.webp' },
+    { id: 4, name: 'Лютик', aka: 'БАРД', description: 'Поэт и друг Геральта. Лучший бард Севера.', weapon: 'Лютня', origin: 'Новиград', image: 'assets/images/S600xU_2x.webp' },
     { id: 5, name: 'Эредін', aka: 'КРАСНЫЙ', description: 'Король Дикой Охоты. Повелитель Мира Эльфов.', weapon: 'Звёздная Пыль', origin: 'Аэртудрассил', image: 'assets/images/w1500_49118093.jpg' },
-    { id: 6, name: 'Трисс', aka: 'РЫЖАЯ', description: 'Чародейка из Каэр Морхен. Советница короля Радовида.', weapon: 'Огненная магия', origin: 'Махакам', image: 'assets/images/i (9).webp' }
+    { id: 6, name: 'Трисс', aka: 'РЫЖАЯ', description: 'Чародейка из Каэр Морхен. Советница короля Радовида.', weapon: 'Огненная магия', origin: 'Махакам', image: 'assets/images/i (1).webp' }
   ];
 
   ngOnInit() {}
--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -178,10 +178,10 @@ export class HomeComponent {
   tr = inject(TranslationService);
   characters = [
     { id: 1, name: 'Геральт из Ривии', aka: 'БЕЛОВОЛОСЫЙ', image: 'assets/images/a5fb7a948cb613a6db107e4e998684cc.jpg' },
-    { id: 2, name: 'Цири', aka: 'ЛАСТОЧКА', image: 'assets/images/h280_39780019.jpg' },
-    { id: 3, name: 'Йеннифер', aka: 'ЛЬВИЦА', image: 'assets/images/a652dcfff029f1f9f5e5e9bf06ee1622.jpg' },
-    { id: 4, name: 'Лютик', aka: 'БАРД', image: 'assets/images/i (8).webp' },
+    { id: 2, name: 'Цири', aka: 'ЛАСТОЧКА', image: 'assets/images/i (10).webp' },
+    { id: 3, name: 'Йеннифер', aka: 'ЛЬВИЦА', image: 'assets/images/i.webp' },
+    { id: 4, name: 'Лютик', aka: 'БАРД', image: 'assets/images/S600xU_2x.webp' },
     { id: 5, name: 'Эредін', aka: 'КРАСНЫЙ', image: 'assets/images/w1500_49118093.jpg' },
-    { id: 6, name: 'Трисс', aka: 'РЫЖАЯ', image: 'assets/images/i (9).webp' }
+    { id: 6, name: 'Трисс', aka: 'РЫЖАЯ', image: 'assets/images/i (1).webp' }
   ];
 }
